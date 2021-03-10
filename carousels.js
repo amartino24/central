5ca5d9a567a700f12f4cdc43b13fac5f3df1f89b
amartino24 <amartino@mroots.com.ar>
2021-03-10 14:03:36 -0300

    Update carousels.js

--- carousels.js
+++ carousels.js
@@ -58,11 +58,22 @@ $(document).ready(function () {
         loop: true,
         nav: true,
         autoplay:true,
+      
         autoplayTimeout:10000,
         navText: [
             '<svg width="50" height="50" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>',
             '<svg width="50" height="50" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>'
-        ]
+        ],
+        responsive:{
+            0:{
+                touchDrag  : true,
+                mouseDrag  : true,
+            },
+            480:{
+                touchDrag  : false,
+                mouseDrag  : false,
+            },
+        }
     });
 
 
