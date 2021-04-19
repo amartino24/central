cfbde666f4983edcfe753d0999bf58668cc6d488
amartino24 <amartino@mroots.com.ar>
2021-04-19 12:36:29 -0300

    a

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'so_rosario_central' );
+define( 'DB_NAME', 'rosariocentral_so' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'rootsat' );
 
 /** MySQL hostname */
-define( 'DB_HOST', '127.0.0.1' );
+define( 'DB_HOST', '192.168.0.10' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
