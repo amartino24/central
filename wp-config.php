24198578efb676816345888123b565470dbcf2d5
amartino24 <amartino@mroots.com.ar>
2021-08-03 18:44:35 -0300

    a

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'rootsat' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '192.168.0.10' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
